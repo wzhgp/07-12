9bff18f1d76de29c2a76a7dbc62b872259cbc09c
wzhgp <wangzehui1596@qq.com>
2017-07-14 11:25:21 +0800

    up

--- 1.js
+++ 1.js
@@ -1,1 +1,6 @@
 var div = document.getElementById('div');
+document.addEventListener('mouseup',up);
+function up(){
+	document.removeEventListener('mousedown',down);
+	document.removeEventListener('mouseup',up);
+}
