65053f1887d30ea00451e0386a714696f5776637
wzhgp <wangzehui1596@qq.com>
2017-07-14 11:20:48 +0800

    down

--- 1.js
+++ 1.js
@@ -1,1 +1,6 @@
 var div = document.getElementById('div');
+div.addEventListener('mousedown',down);
+function down(ev){
+	var disX = ev.clientX - div.offsetLeft;
+	var disY = ev.clientY - div.offsetTop;
+}
