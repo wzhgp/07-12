7cfe2c97e708c5f73ed80cfaa014bdc98474fa62
wzhgp <wangzehui1596@qq.com>
2017-07-14 11:36:56 +0800

    all2

--- 1.js
+++ 1.js
@@ -3,12 +3,14 @@ div.addEventListener('mousedown',down);
 function down(ev){
 	var disX = ev.clientX - div.offsetLeft;
 	var disY = ev.clientY - div.offsetTop;
-document.addEventListener('mousemove',move);
-function move(ev){
-	div.style.left = ev.clientX - disX+'px';
-	div style.top = ev.clientY - disY+'px';
-document.addEventListener('mouseup',up);
-function up(){
-	document.removeEventListener('mousedown',down);
-	document.removeEventListener('mouseup',up);
+	document.addEventListener('mousemove',move);
+	function move(ev){
+		div.style.left = ev.clientX - disX+'px';
+		div style.top = ev.clientY - disY+'px';
+	}
+	document.addEventListener('mouseup',up);
+	function up(){
+		document.removeEventListener('mousedown',down);
+		document.removeEventListener('mouseup',up);
+	}
 }
